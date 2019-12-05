6084f96ec6fcfe64f149468f1918cc41382f7d6d
fanyu <1003798128@qq.com>
2019-12-05 12:00:54 +0800

    上传颜色

--- modulePublic/moduleCommon/src/main/java/com/einyun/app/common/ui/component/viewpager/SliderViewPager.java
+++ modulePublic/moduleCommon/src/main/java/com/einyun/app/common/ui/component/viewpager/SliderViewPager.java
@@ -4,37 +4,17 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.viewpager.widget.ViewPager;
 
 public class SliderViewPager extends ViewPager {
 
-
-    private int preX=0;
-    public SliderViewPager(Context context, AttributeSet attrs) {
-        super(context, attrs);
-
-    }
-    public SliderViewPager(Context context) {
+    public SliderViewPager(@NonNull Context context) {
         super(context);
-
     }
 
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent even) {
-
-        if(even.getAction()==MotionEvent.ACTION_DOWN)
-        {
-            preX=(int) even.getX();
-        }else
-        {
-            if(Math.abs((int)even.getX()-preX)>3)
-            {
-                return true;
-            }else
-            {
-                preX=(int) even.getX();
-            }
-        }
-        return super.onInterceptTouchEvent(even);
+    public SliderViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
     }
 }
