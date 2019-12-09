96770f37f9ac6a506e372be0a5bf3cab9e077fdb
fanyu <1003798128@qq.com>
2019-12-09 16:12:56 +0800

    上传搜索以及选择人员

--- moduleCore/moduleSendOrder/src/main/java/com/einyun/app/pms/sendorder/SendOrderBindAdapter.java
+++ moduleCore/moduleSendOrder/src/main/java/com/einyun/app/pms/sendorder/SendOrderBindAdapter.java
@@ -9,21 +9,23 @@ import android.widget.TextView;
 import androidx.databinding.BindingAdapter;
 
 import com.einyun.app.library.resource.workorder.model.OrderState;
+import com.einyun.app.library.workorder.net.response.GetMappingByUserIdsResponse;
 
 public class SendOrderBindAdapter {
 
     /**
      * 列表状态
+     *
      * @param view
      * @param value
      */
     @BindingAdapter("status")
-    public static void status(TextView view, int value){
-        if(value== OrderState.NEW.getState()){
+    public static void status(TextView view, int value) {
+        if (value == OrderState.NEW.getState()) {
             view.setText(R.string.text_state_new);
-        }else if(value==OrderState.HANDING.getState()||value==OrderState.PROCESSING.getState()){
+        } else if (value == OrderState.HANDING.getState() || value == OrderState.PROCESSING.getState()) {
             view.setText(R.string.text_state_processing);
-        }else if(value==OrderState.CLOSED.getState()){
+        } else if (value == OrderState.CLOSED.getState()) {
             view.setText(R.string.text_state_closed);
         }
 
@@ -31,23 +33,23 @@ public class SendOrderBindAdapter {
 
     /**
      * 派工单详情显示状态
+     *
      * @param view
      * @param value
      */
     @BindingAdapter("order_status")
-    public static void order_status(TextView view, String value){
-        if(TextUtils.isEmpty(value)){
+    public static void order_status(TextView view, String value) {
+        if (TextUtils.isEmpty(value)) {
             return;
         }
-        int state=Integer.parseInt(value);
-        if(state== OrderState.NEW.getState()){
+        int state = Integer.parseInt(value);
+        if (state == OrderState.NEW.getState()) {
             view.setText(R.string.text_state_new);
-        }else if(state==OrderState.HANDING.getState()){
+        } else if (state == OrderState.HANDING.getState()) {
             view.setText(R.string.text_state_processing);
-        } else if(state==OrderState.PROCESSING.getState()){
+        } else if (state == OrderState.PROCESSING.getState()) {
             view.setText(R.string.text_approval_wait);
-        }
-        else if(state==OrderState.CLOSED.getState()){
+        } else if (state == OrderState.CLOSED.getState()) {
             view.setText(R.string.text_state_closed);
         }
 
@@ -55,36 +57,41 @@ public class SendOrderBindAdapter {
 
     /**
      * 派工单详情处理按钮状态
+     *
      * @param view
      * @param value
      */
     @BindingAdapter("button_status")
-    public static void button_status(Button view, String value){
-        if(TextUtils.isEmpty(value)){
+    public static void button_status(Button view, String value) {
+        if (TextUtils.isEmpty(value)) {
             return;
         }
-        int state=Integer.parseInt(value);
-        if(state== OrderState.NEW.getState()){
+        int state = Integer.parseInt(value);
+        if (state == OrderState.NEW.getState()) {
             view.setText(R.string.text_take_order);
-        }else if(state==OrderState.HANDING.getState()){
+        } else if (state == OrderState.HANDING.getState()) {
             view.setText(R.string.text_commit);
-        }else if(state==OrderState.PROCESSING.getState()){
+        } else if (state == OrderState.PROCESSING.getState()) {
             view.setText(R.string.text_approval);
-        }
-        else if(state==OrderState.CLOSED.getState()){
+        } else if (state == OrderState.CLOSED.getState()) {
             view.setVisibility(View.GONE);
         }
     }
 
 
     @BindingAdapter("status")
-    public static void status(ImageView view, int value){
-        if(value== OrderState.NEW.getState()){
+    public static void status(ImageView view, int value) {
+        if (value == OrderState.NEW.getState()) {
             view.setImageResource(R.mipmap.icon_new);
-        }else if(value==OrderState.HANDING.getState()||value==OrderState.PROCESSING.getState()){
+        } else if (value == OrderState.HANDING.getState() || value == OrderState.PROCESSING.getState()) {
             view.setImageResource(R.mipmap.icon_processing);
-        }else if(value==OrderState.CLOSED.getState()){
+        } else if (value == OrderState.CLOSED.getState()) {
             view.setImageResource(R.mipmap.icon_state_closed);
         }
+    }
+
+    @BindingAdapter("setUserName")
+    public static void status(TextView view, GetMappingByUserIdsResponse value) {
+        view.setText(value.getFullname() + "(" + value.getAccount() + ")");
     }
 }
