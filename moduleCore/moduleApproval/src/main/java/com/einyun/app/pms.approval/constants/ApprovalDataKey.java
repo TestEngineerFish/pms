18ec9148a1f5b9a43db4b69e78cb99842c0627fe
shenmingzhao <s15617835337@163.com>
2019-12-30 19:52:35 +0800

    筛选 待审批列表筛选条件修改

--- moduleCore/moduleApproval/src/main/java/com/einyun/app/pms.approval/constants/ApprovalDataKey.java
+++ moduleCore/moduleApproval/src/main/java/com/einyun/app/pms.approval/constants/ApprovalDataKey.java
@@ -4,14 +4,13 @@ public class ApprovalDataKey {
     public static final String APPROVAL_SUMIT_AGREE="agree";
     public static final String APPROVAL_SUMIT_REJECT="reject";
     public static final String APPROVAL_FRAGMENT_REFRESH="APPROVAL_FRAGMENT_REFRESH";
-    public static final String APPROVAL_STATE_HAD_PASS="approve";
-    public static final String APPROVAL_STATE_HAD_UNPASS="reject";
 
 
+    public static final String APPROVAL_STATE_HAD_PASS="approve";//已通过
+    public static final String APPROVAL_STATE_HAD_UNPASS="reject";//已驳回
+    public static final String APPROVAL_STATE_PENDING="submit";// 待审批
+    public static final String APPROVAL_STATE_IN_APPROVAL="in_approval";//审批中
 
-
-    public static final String APPROVAL_STATE_PENDING="submit";
-    public static final String APPROVAL_STATE_IN_APPROVAL="in_approval";
     public static final String APPROVAL_SUMIT_PARMS="APPROVAL_SUMIT_PARMS";
     public static final String APPROVAL_SUMIT_URL="APPROVAL_SUMIT_URL";
 
