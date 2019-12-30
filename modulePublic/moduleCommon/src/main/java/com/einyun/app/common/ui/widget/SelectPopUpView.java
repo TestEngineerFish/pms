21e7232e23083d79bf15887d48aa747b5065c042
zhulufeng <zlf123456>
2019-12-30 15:53:00 +0800

    解决派工单fragment找不到构造方法问题

--- modulePublic/moduleCommon/src/main/java/com/einyun/app/common/ui/widget/SelectPopUpView.java
+++ modulePublic/moduleCommon/src/main/java/com/einyun/app/common/ui/widget/SelectPopUpView.java
@@ -27,6 +27,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Handler;
 
 
 public class SelectPopUpView extends PopupWindow implements View.OnClickListener {
