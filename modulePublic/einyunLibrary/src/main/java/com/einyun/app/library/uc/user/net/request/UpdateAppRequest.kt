5c75054fcd490f9fd95e5db424d42ce4e510e9c4
fanyu <1003798128@qq.com>
2019-12-30 14:26:53 +0800

    上传代码更新

--- modulePublic/einyunLibrary/src/main/java/com/einyun/app/library/uc/user/net/request/UpdateAppRequest.kt
+++ modulePublic/einyunLibrary/src/main/java/com/einyun/app/library/uc/user/net/request/UpdateAppRequest.kt
@@ -6,5 +6,5 @@ import com.einyun.app.base.util.DeviceUtil
 class UpdateAppRequest {
     var type: Int? = 0
     var osVersion: String? = DeviceUtil.getSDKVersion().toString()
-    var version: String? = DeviceUtil.getVersionCode(BasicApplication.getInstance()).toString()
+    var version: String? = DeviceUtil.getVersionName(BasicApplication.getInstance()).toString()
 }
