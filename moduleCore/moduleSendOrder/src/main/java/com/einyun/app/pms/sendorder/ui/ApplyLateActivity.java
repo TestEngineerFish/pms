9cc336e45f83e73c23bc2e63bb72276ec7714ffc
zhulufeng <lufeng.zhu@einyun.com>
2019-12-31 17:41:13 +0800

    修改申请延期界面天数错误问题

--- moduleCore/moduleSendOrder/src/main/java/com/einyun/app/pms/sendorder/ui/ApplyLateActivity.java
+++ moduleCore/moduleSendOrder/src/main/java/com/einyun/app/pms/sendorder/ui/ApplyLateActivity.java
@@ -52,7 +52,7 @@ public class ApplyLateActivity extends BaseApplyPostPoneActivity<SendOrderDetial
         if (RouteKey.KEY_CUSTOMER_COMPLAIN.equals(keyId) || RouteKey.KEY_CUSTOMER_REPAIRS.equals(keyId)){
             viewModel.getApplyDateInfo(proInsId).observe(this,formDataExten -> {
                 binding.applyDate.setText(formDataExten.getDelay_time() + "天");
-                binding.applyNum.setText(formDataExten.getDelay_number() + "次");
+                binding.applyNum.setText(formDataExten.getDelay_sum_time() + "次");
             });
         }
     }
