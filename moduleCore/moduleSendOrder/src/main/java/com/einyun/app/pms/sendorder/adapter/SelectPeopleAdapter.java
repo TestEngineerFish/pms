9c1d5e13522e649cf726c6604310db96fd212bb9
zhulufeng <lufeng.zhu@einyun.com>
2020-01-16 11:07:07 +0800

    修改选择人员接口

--- moduleCore/moduleSendOrder/src/main/java/com/einyun/app/pms/sendorder/adapter/SelectPeopleAdapter.java
+++ moduleCore/moduleSendOrder/src/main/java/com/einyun/app/pms/sendorder/adapter/SelectPeopleAdapter.java
@@ -114,12 +114,11 @@ public class SelectPeopleAdapter extends BaseExpandableListAdapter {
                 @Override
                 public void onClick(View v) {
                     ArrayList<String> list1=new ArrayList<>();
-                    /*list1.add(orgnizationModel.getValue().getId());
-                    list1.add(orgnizationModel.getValue().getParentId());*/
+                    list1.add(orgnizationModelList.getValue().get(0).getParentId());
                     ArrayList<String> list2=new ArrayList<>();
                     list2.add(jobList.getValue().get(childPosition).getId());
                     ARouter.getInstance().build(RouterUtils.ACTIVITY_CHOOSE_DISPOSE_PERSON_SEND_ORDER).withStringArrayList(RouteKey.KEY_ORG_ID_LIST,list1)
-                            .withStringArrayList(RouteKey.KEY_JOB_ID_LIST,list2).navigation();
+                            .withStringArrayList(RouteKey.KEY_ROLE_ID_LIST,list2).navigation();
                 }
             });
         }
