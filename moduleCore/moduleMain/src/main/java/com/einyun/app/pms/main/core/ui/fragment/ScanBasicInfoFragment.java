4a50c16a543041640565c4567bd7a350b7e3a975
shenmingzhao <s15617835337@163.com>
2020-05-27 13:51:23 +0800

    资源扫码 资源管理   环境类别为绿化时 根据取值是否为空判断字段是否显示（拉丁文等前五个属性）

--- moduleCore/moduleMain/src/main/java/com/einyun/app/pms/main/core/ui/fragment/ScanBasicInfoFragment.java
+++ moduleCore/moduleMain/src/main/java/com/einyun/app/pms/main/core/ui/fragment/ScanBasicInfoFragment.java
@@ -100,10 +100,46 @@ public class ScanBasicInfoFragment extends BaseViewModelFragment<FragmentScanBas
                     switch (model.getResourceClassificationPath()) {
                         case "482896"://绿化
                             binding.llGreen.setVisibility(View.VISIBLE);
+                            if (model.getLatin().isEmpty()) {
+                                binding.rlLatin.setVisibility(View.GONE);
+                            }
+                            if (model.getBranch().isEmpty()) {
+                                binding.rlBranch.setVisibility(View.GONE);
+                            }
+                            if (model.getGenus().isEmpty()) {
+                                binding.rlGenus.setVisibility(View.GONE);
+                            }
+                            if (model.getPlaceOfOrigin().isEmpty()) {
+                                binding.rlPlaceOfOrigin.setVisibility(View.GONE);
+                            }
+                            if (model.getHabit().isEmpty()) {
+                                binding.rlHabit.setVisibility(View.GONE);
+                            }
+
                             break;
                         case "482904"://保洁
                             binding.llClean.setVisibility(View.VISIBLE);
                             break;
+
+                            default:
+//                                if (model.getLatin().isEmpty()) {
+//                                    binding.rlLatin.setVisibility(View.GONE);
+//                                }
+//                                if (model.getBranch().isEmpty()) {
+//                                    binding.rlBranch.setVisibility(View.GONE);
+//                                }
+//                                if (model.getGenus().isEmpty()) {
+//                                    binding.rlGenus.setVisibility(View.GONE);
+//                                }
+//                                if (model.getPlaceOfOrigin().isEmpty()) {
+//                                    binding.rlPlaceOfOrigin.setVisibility(View.GONE);
+//                                }
+//                                if (model.getHabit().isEmpty()) {
+//                                    binding.rlHabit.setVisibility(View.GONE);
+//                                }
+//                                binding.llGreen.setVisibility(View.VISIBLE);
+
+                                break;
                     }
 
                     break;
