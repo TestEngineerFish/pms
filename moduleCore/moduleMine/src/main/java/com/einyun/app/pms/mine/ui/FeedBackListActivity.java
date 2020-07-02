2f442574d3678fb10622203969616fd81e87a963
zhulufeng <lufeng.zhu@einyun.com>
2020-07-02 16:53:17 +0800

    意见反馈标题添加字数限制，列表添加下拉刷新

--- moduleCore/moduleMine/src/main/java/com/einyun/app/pms/mine/ui/FeedBackListActivity.java
+++ moduleCore/moduleMine/src/main/java/com/einyun/app/pms/mine/ui/FeedBackListActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
@@ -15,6 +16,7 @@ import com.alibaba.android.arouter.launcher.ARouter;
 import com.einyun.app.base.adapter.RVBindingAdapter;
 import com.einyun.app.base.event.ItemClickListener;
 import com.einyun.app.common.constants.RouteKey;
+import com.einyun.app.common.model.PageUIState;
 import com.einyun.app.common.service.RouterUtils;
 import com.einyun.app.common.service.user.IUserModuleService;
 import com.einyun.app.common.ui.activity.BaseHeadViewModelActivity;
@@ -33,7 +35,6 @@ import java.util.List;
 @Route(path = RouterUtils.ACTIVITY_FEED_LIST)
 public class FeedBackListActivity extends BaseHeadViewModelActivity<ActivityFeedBackListBinding, SettingViewModel> implements ItemClickListener<FeedBackListModel> {
     RVBindingAdapter<ItemFeedBinding, FeedBackListModel> adapter;
-    List<FeedBackModel> feedBackModels = new ArrayList<>();
     @Autowired(name = RouterUtils.SERVICE_USER)
     IUserModuleService userModuleService;
 
@@ -71,20 +72,21 @@ public class FeedBackListActivity extends BaseHeadViewModelActivity<ActivityFeed
         layoutManager.setOrientation(RecyclerView.VERTICAL);
         binding.feedList.setLayoutManager(layoutManager);
         binding.feedList.setAdapter(adapter);
-        for (int i = 0; i < 4; i++) {
-            feedBackModels.add(new FeedBackModel());
-        }
-        viewModel.getFeedBackList(userModuleService.getUserId()).observe(this,data->{
-            if (data!=null){
-                adapter.setDataList(data);
-            }
-        });
+        loadData();
     }
 
     @Override
     protected void initListener() {
         super.initListener();
         adapter.setOnItemClick(this);
+        binding.swipeRefresh.setColorSchemeColors(getColorPrimary());
+        binding.swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                binding.swipeRefresh.setRefreshing(false);
+                loadData();
+            }
+        });
     }
 
     @Override
@@ -103,4 +105,17 @@ public class FeedBackListActivity extends BaseHeadViewModelActivity<ActivityFeed
         ARouter.getInstance().build(RouterUtils.ACTIVITY_FEED_DETAIL).withString(RouteKey.KEY_FEED_ID,data.getId())
                 .navigation();
     }
+
+    private void loadData() {
+        viewModel.getFeedBackList(userModuleService.getUserId()).observe(this, data -> {
+            if (data == null || data.size() == 0) {
+                binding.pageState.setPageState(PageUIState.EMPTY.getState());
+                return;
+            }
+            if (data != null) {
+                adapter.setDataList(data);
+            }
+        });
+    }
+
 }
